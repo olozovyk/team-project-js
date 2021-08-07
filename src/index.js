7c43edc8036af8d9a17ebd1f4313af8a2cf1eee5
Oleg Lozovik <oldisc3@gmail.com>
2021-08-08 00:32:23 +0300

    getMovies() refactoring

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './sass/main.scss';
 import 'modern-normalize/modern-normalize.css';
 import { getMovies } from './js/fetch';
 
-getMovies({ page: 1, query: 'животные' }).then(console.log);
+getMovies({}).then(console.log);
 //Для того, чтобы получить результаты поиска, добавить, как аргумент ф-ции {query: 'запрос'}
-//Для панигации добавить {page: номер страницы}
+//Для пагинации добавить {page: номер страницы}
