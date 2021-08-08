65d2fd59f1b477fa294b83794192f4bbbe9234e9
Vladyslav-Motuz <78124848+Vladyslav-Motuz@users.noreply.github.com>
2021-08-08 18:25:51 +0300

    fix templating

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import 'modern-normalize/modern-normalize.css';
 import { getMovies } from './js/fetch';
 import { genresSet, dataSet } from './js/templatingSettings';
 import templatingOneFilm from './templates/templatingOneFilm.hbs';
-getMovies({ page: 1, query: 'животные' }).then(films => {
-    films.forEach(film => {
+getMovies().then(films => {
+    const filmsArr = films.map(film => {
         const filmGenres = genresSet(film.genreNames);
         const filmDate = dataSet(film.release_date);
-        console.log(templatingOneFilm({ film, filmGenres, filmDate }));
-    });
-});
+        return {...film, filmGenres, filmDate}
+    })
+    return filmsArr;
+}).then(films => {
+    console.log(templatingOneFilm(films));
+})
 //Для того, чтобы получить результаты поиска, добавить, как аргумент ф-ции {query: 'запрос'}
 //Для панигации добавить {page: номер страницы}
