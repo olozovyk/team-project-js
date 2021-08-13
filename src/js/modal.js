98a7de198391a37f07898e92cece1a75179b5f79
Oleg Lozovik <oldisc3@gmail.com>
2021-08-13 15:43:17 +0300

    adding to lists

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,7 +5,7 @@ import { refs } from './refs';
 const modalContentEl = document.querySelector('#modal-content');
 
 const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies')) || [];
-const queueMovies = [];
+const queueMovies = JSON.parse(localStorage.getItem('queueMovies')) || [];
 
 export function controlModal() {
   const openModalBtnEl = document.querySelector('[data-modal-open]');
@@ -21,14 +21,14 @@ export function controlModal() {
     if (event.target.nodeName === 'UL') {
       return;
     }
-    openModal(modalEl);
+    // openModal(modalEl);
 
     const listItem = event.target.closest('LI');
     if (!listItem) {
       return;
     }
 
-    const movieId = listItem.dataset.id;
+    const movieId = Number(listItem.dataset.id);
     if (!movieId) {
       return;
     }
@@ -36,6 +36,9 @@ export function controlModal() {
     getMovieById(movieId)
       .then(movie => {
         modalContentEl.innerHTML = movieTemplate(movie);
+      })
+      .then(() => {
+        openModal(modalEl);
       })
 
       /* BUTTONS */
@@ -46,31 +49,56 @@ export function controlModal() {
           return;
         }
 
-        const liContent = listItem.innerHTML;
-        console.log(liContent);
+        controlBtnStyle({
+          button: modalWatchedBtn,
+          list: watchedMovies,
+          movieId,
+          listType: 'watched',
+        });
+        controlBtnStyle({ button: modalQueueBtn, list: queueMovies, movieId, listType: 'queue' });
 
         modalWatchedBtn.addEventListener('click', () => {
-          let indexMovie = null;
-
-          watchedMovies.forEach((item, idx) => {
-            if (Number(item.id) === Number(movieId)) {
-              indexMovie = idx;
-            }
-          });
-
-          if (indexMovie !== null) {
+          const indexMovie = watchedMovies.indexOf(Number(movieId));
+          if (indexMovie > -1) {
             watchedMovies.splice(indexMovie, 1);
             localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
-            console.table(watchedMovies);
-            indexMovie = null;
-          } else {
-            const movieObj = { id: movieId, liContent };
-            watchedMovies.push(movieObj);
-            localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
-            console.table(watchedMovies);
+            controlBtnStyle({
+              button: modalWatchedBtn,
+              list: watchedMovies,
+              movieId,
+              listType: 'watched',
+            });
+            return;
           }
+          watchedMovies.push(Number(movieId));
+          localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+          controlBtnStyle({
+            button: modalWatchedBtn,
+            list: watchedMovies,
+            movieId,
+            listType: 'watched',
+          });
+        });
+
+        modalQueueBtn.addEventListener('click', () => {
+          const indexMovie = queueMovies.indexOf(Number(movieId));
+          if (indexMovie > -1) {
+            queueMovies.splice(indexMovie, 1);
+            localStorage.setItem('queueMovies', JSON.stringify(queueMovies));
+            controlBtnStyle({
+              button: modalQueueBtn,
+              list: queueMovies,
+              movieId,
+              listType: 'queue',
+            });
+            return;
+          }
+          queueMovies.push(Number(movieId));
+          localStorage.setItem('queueMovies', JSON.stringify(queueMovies));
+          controlBtnStyle({ button: modalQueueBtn, list: queueMovies, movieId, listType: 'queue' });
         });
       });
+
     /* BUTTONS */
   }
 
@@ -85,5 +113,16 @@ export function controlModal() {
     modalContentEl.innerHTML = '';
     document.body.style.overflow = 'auto';
     document.body.style.width = '100%';
+  }
+
+  function controlBtnStyle({ button, list, movieId, listType }) {
+    const btnInList = list.includes(movieId);
+    if (btnInList) {
+      button.classList.add('in-list');
+      button.textContent = listType === 'watched' ? 'REMOVE FROM WATCHED' : 'REMOVE FROM QUEUE';
+      return;
+    }
+    button.classList.remove('in-list');
+    button.textContent = listType === 'watched' ? 'ADD TO WATCHED' : 'ADD TO QUEUE';
   }
 }
